dcce306213672b0d8db618b47eb11803c831f18f
dirane <diranengala@gmail.com>
2017-08-09 12:53:36 +0000

    fetching each article by id

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title':'Article one',
+            'body':'lorem dkjkjlkjl',
+            'author':'My author',
+            'create_date':'09/08/2017'
+        },
+
+        {
+            'id': 2,
+            'title':'Article two',
+            'body':'lorem dkjkjlkjl',
+            'author':'second author',
+            'create_date':'09/08/2017'
+        },
+        {
+            'id': 3,
+            'title': 'Article three',
+            'body': 'lorem dkjkjlkjl',
+            'author': 'third author',
+            'create_date': '09/08/2017'
+        }
+    ]
+    return articles
